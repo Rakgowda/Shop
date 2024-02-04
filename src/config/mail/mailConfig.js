4fa625c5ba56812a0be1c121e3de4556d59c78af
rakshithgowda <rakshithgowdakv@gmail.com>
2024-02-05 00:00:48 +0530

    new changes

--- src/config/mail/mailConfig.js
+++ src/config/mail/mailConfig.js
@@ -1,4 +1,6 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
+const { getOkMsg } = require('../../utils/statusCodeUtil');
+const { throwNewError } = require('../../utils/errorHandlerUtils');
 
 exports.mailTransporter = nodemailer.createTransport({
     service: 'gmail',
@@ -17,15 +19,16 @@ exports.mailTransporter = nodemailer.createTransport({
 // };
  
 
-// exports.sendMail = (req, res, next)=>{
-//     mailTransporter.sendMail(mailDetails, function(err, data) {
-//         if(err) {
-//             console.log(err);
-//             console.log('Error Occurs');
-//             res.send("error while sending")
-//         } else {
-//             res.send(data)
-//             console.log('Email sent successfully');
-//         }
-//     });
-// }
+exports.sendMail = (req, res, next)=>{
+    mailTransporter.sendMail(mailDetails, function(err, data) {
+        if(err) {
+            console.log(err);
+            console.log('Error Occurs');
+            // res.send("error while sending")
+            throwNewError(err,statusCode.INTERNAL_SERVER_ERROR)
+        } else {
+            // res.send(getOkMsg("Password reset link is sent to email, please check your email."))
+            return true;
+        }
+    });
+}
